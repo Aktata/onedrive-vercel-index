e23659ec10153e04cc50e8d46fefa7b377ddf9bc
UrielGGs <wugang000000@gmail.com>
2023-02-22 18:11:15 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,8 +52,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/UrielGGs',
+      name: 'Telegram',
+      link: 'https://t.me/Aktatat',
     },
   ],
 
